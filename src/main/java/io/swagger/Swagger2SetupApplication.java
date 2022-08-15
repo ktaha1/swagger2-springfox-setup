7e07d7e7b97c87366a8c31f852bfe77e5e7f97f6
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-15 23:18:56 +0100

    Initial commit

---
+++ src/main/java/io/swagger/Swagger2SetupApplication.java
@@ -0,0 +1,13 @@
+package io.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2SetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2SetupApplication.class, args);
+	}
+
+}
